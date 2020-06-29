4b971f66ae87fef07699a139ac70c1e42f41485b
omrobbie <putu.roby@yahoo.com>
2020-06-29 16:26:01 +0800

    setup table view with dummy data

--- ios-todolist-userdefaults/ViewController.swift
+++ ios-todolist-userdefaults/ViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
     }
 }
